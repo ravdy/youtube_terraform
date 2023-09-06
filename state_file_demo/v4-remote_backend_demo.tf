1b5e97a3d7a5776c5986f678cff9b52d0c250226
Ravi Shankar <30906888+ravdy@users.noreply.github.com>
2023-09-06 19:18:50 +0530

    Update v4-remote_backend_demo.tf

--- state_file_demo/v4-remote_backend_demo.tf
+++ state_file_demo/v4-remote_backend_demo.tf
@@ -11,9 +11,6 @@ terraform {
     bucket         = "valaxy-terraform-state-file"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    # Replace this with your DynamoDB table name!
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt        = true
   }
 
 }
